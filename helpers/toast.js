509d457d6e91c4006869c5850e2a1d37378e6e96
sakura-flutter <736543307@qq.com>
2020-09-09 14:07:44 +0800

    feat:蓝湖、Toast

---
+++ helpers/toast.js
@@ -0,0 +1,136 @@
+// ==UserScript==
+// @name         Toast
+// @version      1.0.0
+// @description  Toast 可以直接Toast[type] 调用
+// @author       sakura-flutter
+// @namespace    https://github.com/sakura-flutter
+// @compatible   chrome >= 80
+// @compatible   firefox >= 75
+// ==/UserScript==
+
+/* global Vue Toast */
+
+(function() {
+  'use strict'
+
+  window.Toast = function(options) {
+      if (typeof options === 'string') {
+          options = { content: options }
+      }
+      // 参数
+      options = Object.assign({
+          content: '',
+          type: 'info',
+          duration: 3000, // 0不会自动关闭
+      }, options)
+
+      const toast = new Vue({
+          template: `
+           <transition name="fade" appear @before-enter="beforeEnter" @enter="enter" @leave="leave" @after-leave="afterLeave">
+             <div
+               :style="colour"
+               style="position:fixed; z-index:99999; top:80px; left:50%; padding:8px 16px; font-size:14px; box-shadow:0 2px 3px rgba(0,0,0,.1); transition:all .3s ease-in-out;"
+               v-if="visible"
+               v-html="content"
+             >
+             </div>
+           </transition>
+         `,
+          data() {
+              return {
+                  content: options.content,
+                  type: options.type,
+                  visible: true,
+              }
+          },
+          computed: {
+              // 颜色
+              colour() {
+                  switch(this.type) {
+                      case 'info':
+                          return {
+                              color: '#2e8bf0',
+                              background: '#f0faff',
+                              border: '1px solid #d4eeff',
+                          }
+                      case 'success':
+                          return {
+                              color: '#19bf6c',
+                              background: '#edfff3',
+                              border: '1px solid #bbf2cf',
+                          }
+                      case 'warning':
+                          return {
+                              color: '#f90',
+                              background: '#fff9e6',
+                              border: '1px solid #ffe7a3',
+                          }
+                      case 'error':
+                          return {
+                              color: '#ed3f13',
+                              background: '#ffefe6',
+                              border: '1px solid #ffcfb8',
+                          }
+                  }
+              }
+          },
+          methods: {
+              // export-api
+              // 关闭
+              close() {
+                  this.visible = false
+              },
+              beforeEnter(el) {
+                  el.style.opacity = 0
+                  el.style.transform = 'translate(-50%, -10%)'
+              },
+              enter(el, done) {
+                  setTimeout(() => {
+                      el.style.opacity = 1
+                      el.style.transform = 'translate(-50%, 0)'
+                  })
+              },
+              leave(el, done) {
+                  setTimeout(() => {
+                      el.style.opacity = 0
+                      el.style.transform = 'translate(-50%, 30%)'
+                  })
+              },
+              afterLeave () {
+                  this.$destroy()
+                  this.$el.parentNode.removeChild(this.$el)
+              },
+          },
+      }).$mount()
+      document.body.appendChild(toast.$el)
+
+      if (options.duration > 0) {
+          setTimeout(() => {
+              toast.visible = false
+          }, options.duration)
+      }
+
+      return {
+          // 关闭
+          close: toast.close,
+      }
+  }
+
+  ;['info', 'success', 'warning', 'error'].forEach(type => {
+      Toast[type] = function(options) {
+          if (typeof options === 'string') {
+              options = { content: options }
+          }
+          options = {
+              ...options,
+              type,
+          }
+          return Toast(options)
+      }
+  })
+
+  Toast.config = function({Vue}) {
+      Vue = Vue
+  }
+
+})();
---
+++ lanhu/index.js
@@ -0,0 +1,105 @@
+// ==UserScript==
+// @name         蓝湖 lanhu
+// @version      1.0.1
+// @description  自动填充填写过的产品密码(不是蓝湖账户)；查看产品页面窗口改变后帮助侧边栏更新高度
+// @author       sakura-flutter
+// @namespace    https://github.com/sakura-flutter
+// @compatible   chrome >= 80
+// @compatible   firefox >= 75
+// @run-at       document-start
+// @match        https://lanhuapp.com/web/
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  const $ = document.querySelector.bind(document)
+
+  /* 填充密码 */
+  const oldPushState = unsafeWindow.history.pushState
+  unsafeWindow.history.pushState = function(...args) {
+      oldPushState.apply(unsafeWindow.history, args)
+      console.warn('pushState')
+      setTimeout(autofillPassword, 500)
+  }
+
+  unsafeWindow.addEventListener('DOMContentLoaded', () => {
+      console.warn('DOMContentLoaded')
+      setTimeout(autofillPassword, 500)
+  })
+
+  function autofillPassword() {
+      if (!location.hash.startsWith('#/item/project/door')) return
+      const queryString = location.hash.includes('?') ? location.hash.split('?')[1] : ''
+      if (!queryString) return
+      const pid = new URLSearchParams(queryString).get('pid')
+      // 确认登录按钮   密码框
+      const [confirmEl, passwordEl] = [
+          $('#project-door .mu-raised-button-wrapper'),
+          $('#project-door .pass input'),
+      ]
+      if (!pid || !confirmEl || !passwordEl) return
+
+      const pidPassword = GM_getValue('passwords', {})[pid]
+      if (pidPassword) {
+          passwordEl.value = pidPassword
+          confirmEl.click()
+      }
+
+      // 蓝湖刷新会重新跳转到输入密码页 pushState与DOMContentLoaded 可能会导致重复注册事件
+      // 标记一下事件
+      if (confirmEl.dataset.addedHandler) return
+      confirmEl.dataset.addedHandler = '1'
+
+      // 点击后保存密码
+      confirmEl.addEventListener('mousedown', savePassword)
+      // 回车键保存密码
+      passwordEl.addEventListener('keydown', event => {
+          if (event.keyCode !== 13) return
+          savePassword()
+      })
+
+      function savePassword() {
+          const savedPassword = GM_getValue('passwords', {})
+          const password = passwordEl.value
+          GM_setValue('passwords', {
+              ...savedPassword,
+              [pid]: password,
+          })
+      }
+  }
+
+  /* 更新侧边栏高度 */
+  window.addEventListener('resize', throttle(function() {
+      const barEl = $('.flexible-bar')
+      const modalEl = $('.flexible-modal')
+      if (!barEl || !modalEl) return
+      barEl.dispatchEvent(new MouseEvent('mousedown'))
+      modalEl.dispatchEvent(new MouseEvent('mouseup'))
+  }, 150))
+
+
+
+  function throttle(fn, delay) {
+      var t = null,
+          begin = new Date().getTime();
+
+      return function (...args) {
+          var _self = this,
+              cur = new Date().getTime();
+
+          clearTimeout(t);
+
+          if (cur - begin >= delay) {
+              fn.apply(_self, args);
+              begin = cur;
+          } else {
+              t = setTimeout(function () {
+                  fn.apply(_self, args);
+              }, delay);
+          }
+      }
+  }
+})();
