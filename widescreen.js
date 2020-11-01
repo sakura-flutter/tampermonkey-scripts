1d44b9375b0d6038475928a2049fe8295e75f05e
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-11-01 18:42:10 +0800

    Updates

--- widescreen.js
+++ widescreen.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         论坛文章页宽屏
-// @version      1.14.1
+// @version      1.14.2
 // @description  适配了半次元、微信公众号、知乎、掘金、简书、贴吧、百度搜索、搜狗搜索、segmentfault、哔哩哔哩、微博、豆瓣电影、今日头条
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts/commits/master/src/aggregation/widescreen.js
@@ -833,7 +833,7 @@ handlers.set('tiebaForum', function () {
           display: flex;
         }
         .threadlist_detail .pull_left {
-          flex: 1;
+          flex: auto;
         }
         .threadlist_detail .pull_left .threadlist_abs {
           width: 97%;
