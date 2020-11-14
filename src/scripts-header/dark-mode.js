f239575d73a0c7da6c0bf2f12cda1e63f84d7511
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-11-15 02:23:48 +0800

    feat(dark-mode)

---
+++ src/scripts-header/dark-mode.js
@@ -0,0 +1,15 @@
+module.exports = isProd =>
+`// ==UserScript==
+// @name         Dark Mode 暗黑模式
+// @version      0.0.1
+// @description  将网页变更为暗黑显示，不适合有背景图的网站
+// @author       sakura-flutter
+// @namespace    https://github.com/sakura-flutter/tampermonkey-scripts/commits/master/src/scripts/dark-mode
+// @license      MIT
+// @compatible   chrome >= Latest
+// @compatible   firefox >= Latest
+// @run-at       document-start
+// @match        *://*/*
+// @grant        GM_addStyle
+// ==/UserScript==
+`
---
+++ src/scripts/dark-mode/index.js
@@ -0,0 +1,11 @@
+GM_addStyle(`
+  html {
+    filter: invert(1) hue-rotate(180deg);
+    background: #fff !important;
+  }
+
+  html img,
+  html video {
+    filter: invert(1) hue-rotate(180deg);
+  }
+`)
