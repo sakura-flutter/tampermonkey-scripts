bcc68358ee2a14199b3c3211c1ec39ced35460e5
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-11-18 23:51:21 +0800

    style

--- src/scripts-header/redirect.js
+++ src/scripts-header/redirect.js
@@ -1,7 +1,7 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         redirect 自动跳转到目标链接
-// @version      0.0.1
+// @version      1.0.0
 // @description  自动跳转(重定向)到目标链接，免去点击步骤
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts/commits/master/src/scripts/redirect
