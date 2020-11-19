569f5e012f093692d13b45b8ed89658ff6ebfb35
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-11-19 16:47:32 +0800

    style

--- src/scripts/widescreen/sites/www-baidu-com/index.js
+++ src/scripts/widescreen/sites/www-baidu-com/index.js
@@ -7,7 +7,6 @@ export const baidu = ({ store, createControl }) => ({
       const styleSheet = GM_addStyle(styles)
 
       readyState.interactive(() => {
-        console.log(document.readyState)
         const template = document.createElement('template')
         template.appendChild(styleSheet)
         // 搜索时百度会清除head这里将样式插入一次到body
