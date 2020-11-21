2d7eb9acadff9de6a8567e031b50bce36192ebdb
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-11-21 19:13:41 +0800

    style: 删除冗余方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -30,25 +30,7 @@ export function once(fn) {
 }
 
 /**
- * 有些脚本是在document-start执行的，安全地获得document
- * @param {fn} cb
- */
-export function documentLoaded(cb) {
-  document.body
-    ? cb()
-    : window.addEventListener('DOMContentLoaded', cb)
-}
-
-/**
  * 延时
  * @param {number} ms 毫秒数
  */
 export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-export function toFormData(params = {}) {
-  const formData = new FormData()
-  for (const [key, value] of Object.entries(params)) {
-    formData.append(key, value)
-  }
-  return formData
-}
