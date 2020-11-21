bfec10c519d653f9371691e57abf1335c48e5083
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-11-21 14:14:52 +0800

    feat(redirect): 适配QQ邮箱

--- src/scripts-header/redirect.js
+++ src/scripts-header/redirect.js
@@ -1,8 +1,8 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         redirect 自动跳转到目标链接
-// @version      1.0.0
-// @description  自动跳转(重定向)到目标链接，免去点击步骤
+// @version      1.1.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts/commits/master/src/scripts/redirect
 // @license      GPL-3.0
@@ -12,5 +12,6 @@ module.exports = isProd =>
 // @match        *://www.jianshu.com/go-wild*
 // @match        *://link.zhihu.com/*
 // @match        *://t.cn/*
+// @match        *://mail.qq.com/cgi-bin/*
 // ==/UserScript==
 `
