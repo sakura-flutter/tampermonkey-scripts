7e022b8164db05cb2c400928f7c3835d383741ad
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-11-24 20:46:02 +0800

    style(lanhu)

---
+++ src/scripts/lanhu/bar.js
@@ -0,0 +1,18 @@
+import { throttle } from '@/utils'
+import { $ } from '@/utils/selector'
+
+/* 更新侧边栏高度 */
+function fixBarHeight() {
+  window.addEventListener('resize', throttle(function() {
+    if (!location.hash.startsWith('#/item/project/product')) return
+    const barEl = $('.flexible-bar')
+    const modalEl = $('.flexible-modal')
+    if (!barEl || !modalEl) return
+    barEl.dispatchEvent(new MouseEvent('mousedown'))
+    modalEl.dispatchEvent(new MouseEvent('mouseup'))
+  }, 150))
+}
+
+export {
+  fixBarHeight,
+}
