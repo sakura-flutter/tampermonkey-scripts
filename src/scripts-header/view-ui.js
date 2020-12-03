9fb2580db9f0039f1ed20c85e8815528345303c2
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-12-03 23:43:31 +0800

    docs: 更新链接

--- src/scripts-header/view-ui.js
+++ src/scripts-header/view-ui.js
@@ -4,7 +4,7 @@ module.exports = isProd =>
 // @version      1.0.0
 // @description  隐藏文档中菜单项：Pro、物料
 // @author       sakura-flutter
-// @namespace    https://github.com/sakura-flutter/tampermonkey-scripts/commits/master/src/scripts/view-ui
+// @namespace    https://github.com/sakura-flutter/tampermonkey-scripts/commits/main/src/scripts/view-ui
 // @license      GPL-3.0
 // @compatible   chrome >= Latest
 // @compatible   firefox >= Latest
