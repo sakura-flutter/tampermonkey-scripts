c49a50396b74a3cee9855e7a4fdc704e12e23713
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-12-09 23:55:41 +0800

    release(element-ui)

--- src/scripts-header/element-ui.js
+++ src/scripts-header/element-ui.js
@@ -1,7 +1,7 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         Element UI文档辅助
-// @version      1.0.0
+// @version      1.0.1
 // @description  在Element UI文档中增加示例目录导航，同时支持v2与v3(element-plus)版本，类似于Ant右侧悬浮的导航
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
