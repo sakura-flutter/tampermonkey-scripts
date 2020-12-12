42499f6e3b240369574f50e8b5630eeefdfe8c19
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-12-12 15:52:44 +0800

    feat: useGMvalue增加deep

--- src/composables/use-gm-value.js
+++ src/composables/use-gm-value.js
@@ -4,12 +4,17 @@ import { onUnmounted, ref, watch } from 'vue'
  * 同GM_getValue、GM_setValue
  * @param {string} name
  * @param {any} defaultValue
- * @param {boolean} listening
+ * @param {boolean | object} options: listening, deep
  * @return {any}
  */
-export function useGMvalue(name, defaultValue, listening = true) {
+export function useGMvalue(name, defaultValue, _options) {
+  const { listening, deep } = Object.assign({
+    listening: typeof _options === 'boolean' ? _options : true,
+    deep: false,
+  }, _options)
+
   const value = ref(GM_getValue(name, defaultValue))
-  watch(value, () => { GM_setValue(name, value.value) })
+  watch(value, () => { GM_setValue(name, value.value) }, { deep })
 
   if (listening) {
     onUnmounted(() => {
