077f41b40de3942d420a57526aceda54d96345ca
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-12-14 15:24:28 +0800

    Updates

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         redirect 自动跳转到目标链接
-// @version      1.2.2
-// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC
+// @version      1.3.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -13,6 +13,7 @@
 // @match        *://t.cn/*
 // @match        *://mail.qq.com/cgi-bin/*
 // @match        *://c.pc.qq.com/middlem.html*
+// @match        *://app.yinxiang.com/OutboundRedirect.action*
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
@@ -1854,7 +1855,13 @@ const qqMail = () => ({
 const qqPC = () => ({
   link: parse().pfurl
 });
+;// CONCATENATED MODULE: ./src/scripts/redirect/sites/app-yinxiang-com.js
+
+const yinxiang = () => ({
+  link: parse().dest
+});
 ;// CONCATENATED MODULE: ./src/scripts/redirect/sites/index.js
+
 
 
 
@@ -1883,6 +1890,10 @@ const sites = [{
   name: 'QQPC',
   test: /c\.pc\.qq.com\/middlem\.html/,
   use: qqPC
+}, {
+  name: '印象笔记',
+  test: /app\.yinxiang\.com\/OutboundRedirect/,
+  use: yinxiang
 }];
 /* harmony default export */ const redirect_sites = (sites);
 ;// CONCATENATED MODULE: ./src/scripts/redirect/index.js
