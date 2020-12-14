2658bfd48bc88b146eeadbeb95a803d8d906c27a
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2020-12-14 15:23:29 +0800

    feat(redirect): 适配印象笔记

--- src/scripts-header/redirect.js
+++ src/scripts-header/redirect.js
@@ -1,8 +1,8 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         redirect 自动跳转到目标链接
-// @version      1.2.2
-// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC
+// @version      1.3.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -14,5 +14,6 @@ module.exports = isProd =>
 // @match        *://t.cn/*
 // @match        *://mail.qq.com/cgi-bin/*
 // @match        *://c.pc.qq.com/middlem.html*
+// @match        *://app.yinxiang.com/OutboundRedirect.action*
 // ==/UserScript==
 `
