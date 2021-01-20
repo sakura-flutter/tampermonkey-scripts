b3a6c364d7d4b251b863f354f5c7865dfb7e7eb9
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-01-20 15:46:47 +0800

    update(lanhu): 删除兼容

--- src/scripts/lanhu/index.js
+++ src/scripts/lanhu/index.js
@@ -7,7 +7,6 @@ import { fixBarHeight } from './bar'
 function main() {
   if (!checker()) return
 
-  updateStorage()
   fixBarHeight()
 
   const app = $('.whole').__vue__
@@ -22,26 +21,6 @@ function main() {
     // 蓝湖title是动态获取的，可能有延时，延时处理
     setTimeout(recorder.record, 500)
   }, { immediate: true })
-}
-
-// 将已保存的旧格式替换为新数据格式
-function updateStorage() {
-  const records = GM_getValue('records')
-  if (!records) return
-  let hasDiff = false
-  const newRecords = records.map(record => {
-    if (record.href) return record
-    hasDiff = true
-    const PATH = 'https://lanhuapp.com/web/'
-    const { hash, queryString, ...rest } = record
-    return {
-      href: PATH + hash + '?' + queryString,
-      ...rest,
-    }
-  })
-  if (hasDiff) {
-    GM_setValue('records', newRecords)
-  }
 }
 
 main()
