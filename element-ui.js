f7da5aa1ae846a17400534339ff7f8a4b5516a0f
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-01-28 21:58:55 +0800

    Updates

--- element-ui.js
+++ element-ui.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name         Element UI文档辅助
-// @version      1.0.2
+// @version      1.0.3
 // @description  在Element UI文档中增加示例目录导航，同时支持v2与v3(element-plus)版本，类似于Ant右侧悬浮的导航
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
-// @compatible   chrome >= Latest
-// @compatible   firefox >= Latest
+// @compatible   chrome Latest
+// @compatible   firefox Latest
+// @compatible   edge Latest
 // @match        https://element-plus.gitee.io/*
 // @match        https://element-plus.org/*
 // @match        https://element.eleme.cn/*
 // @match        https://element.eleme.io/*
-// @require      https://cdn.jsdelivr.net/npm/vue@3.0.4/dist/vue.runtime.global.prod.js
+// @require      https://cdn.jsdelivr.net/npm/vue@3/dist/vue.runtime.global.prod.js
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
@@ -719,8 +720,9 @@ async function main() {
     await sleep(500);
   }
 
-  if (!instance) return;
-  warn(instance);
+  warn(instance); // element-plus 已支持
+
+  if ($('#app').__vue_app__) return;
   const catalogue = new Catalogue({
     // 注意：选择器要同时兼容element与element plus文档
     scope: '.page-container .page-component__content section.element-doc > h3'
