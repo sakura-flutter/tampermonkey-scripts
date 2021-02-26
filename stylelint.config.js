e6ce14c50063701d260bd7be90bb0f961f9b4944
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-02-26 20:34:32 +0800

    style: stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,4 +3,12 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-sass-guidelines',
   ],
+  rules: {
+    'selector-class-pattern': null, // 名称限制
+    'max-nesting-depth': null, // 嵌套的深度
+    'selector-max-id': null, // ID选择器数量
+    'selector-max-compound-selectors': null, // 选择器中复合选择器的数量
+    'declaration-property-value-disallowed-list': null, // 在声明中指定不允许的属性和值的列表
+    'selector-no-qualifying-type': null, // 禁止按类型限定选择器
+  },
 }
