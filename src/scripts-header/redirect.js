0fed94fdda7b372db1d923c332bc7654b345ca1c
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-02-26 21:30:18 +0800

    feat(redirect): 适配360doc

--- src/scripts-header/redirect.js
+++ src/scripts-header/redirect.js
@@ -1,8 +1,8 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         redirect 外链跳转
-// @version      1.10.0
-// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣
+// @version      1.11.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -23,5 +23,6 @@ module.exports = isProd =>
 // @match        *://developers.weixin.qq.com/community/middlepage/href*
 // @match        *://www.itdaan.com/link/*
 // @match        *://www.douban.com/link2/*
+// @match        *://www.360doc.com/content/*
 // ==/UserScript==
 `
