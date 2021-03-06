31b836677a34980e1bbc95723a4d9590f2bb6d13
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-03-06 13:53:58 +0800

    feat(mdn-web-docs): 跟随网页更新，新增语言切换按钮

--- src/scripts/mdn-web-docs/index.js
+++ src/scripts/mdn-web-docs/index.js
@@ -1,6 +1,11 @@
 import { $ } from '@/utils/selector'
 import { warn } from '@/utils/log'
 
+const docsLang = matchLang(location.pathname)
+const supports = getSupports()
+warn(docsLang)
+warn(supports)
+
 function main() {
   window.addEventListener('click', event => {
     // 标记是否自行切换语言
@@ -9,17 +14,50 @@ function main() {
     }
   }, true)
 
-  const docsLang = matchLang(location.pathname)
-  warn(docsLang)
+  changeLang()
+  addLangButton()
+}
+
+function changeLang() {
   if (isChinese(docsLang)) return
   // 是否自行切换过语言
   if (sessionStorage.getItem('hand-control-language') === 'true') return
 
-  const supports = getSupports()
-  warn(supports)
   for (const item of supports) {
     isChinese(matchLang(item)) && location.replace(item)
   }
+}
+
+function addLangButton() {
+  const values = [] // 中英 排序
+  for (const item of supports) {
+    const lang = matchLang(item)
+    if (isChinese(lang)) {
+      values[0] = item
+    } else if (isEnglish(lang)) {
+      values[1] = item
+    }
+  }
+  warn(values)
+  if (values.length < 2) return
+
+  const a = document.createElement('a')
+  a.innerText = '中-英'
+  a.href = isChinese(docsLang) ? values[1] : values[0]
+  a.classList.add('button')
+  a.style = [
+    'position: fixed',
+    'right: 0',
+    'bottom: 15vh',
+    'min-height: auto',
+    'padding: 0px 2px',
+    'font-size: 12px',
+    'letter-spacing: 2px',
+  ].join(';')
+  a.onclick = function() {
+    sessionStorage.setItem('hand-control-language', true)
+  }
+  document.body.append(a)
 }
 
 function matchLang(str) {
@@ -32,8 +70,12 @@ function isChinese(lang) {
   return /zh-cn/i.test(lang)
 }
 
+function isEnglish(lang) {
+  return /en-US/i.test(lang)
+}
+
 function getSupports() {
-  return [...$('#select_language').options].map(opt => opt.value)
+  return [...$('#language-selector').options].map(opt => opt.value)
 }
 
 main()
