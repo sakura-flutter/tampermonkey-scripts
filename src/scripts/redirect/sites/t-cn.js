da23b27df4e52178d6857130492f7439efb76e47
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-03-10 11:41:38 +0800

    feat(redirect): 尝试跳转被weibo停止访问的链接

--- src/scripts/redirect/sites/t-cn.js
+++ src/scripts/redirect/sites/t-cn.js
@@ -1,4 +1,10 @@
-export const weibo = () => ({
-  selector: '.open-url a[href]',
-  attr: 'href',
-})
+import { $ } from '@/utils/selector'
+
+export const weibo = async () => {
+  let link = $('.open-url a[href]')?.href
+  link ||= await fetch(location.href).then(response => response.headers.get('location'))
+
+  return {
+    link,
+  }
+}
