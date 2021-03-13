fee179ca91c31358a05e0645dda4665d23ea1888
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-03-13 16:19:40 +0800

    feat(redirect): 适配pixiv

--- src/scripts-header/redirect.js
+++ src/scripts-header/redirect.js
@@ -1,8 +1,8 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         redirect 外链跳转
-// @version      1.12.0
-// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆
+// @version      1.13.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -24,5 +24,6 @@ module.exports = isProd =>
 // @match        *://www.itdaan.com/link/*
 // @match        *://www.douban.com/link2/*
 // @match        *://www.360doc.com/content/*
+// @match        *://www.pixiv.net/jump.php*
 // ==/UserScript==
 `
