636831ab77e8c172dd90469871e639f263b83a6c
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-03-14 02:13:21 +0800

    feat: github

---
+++ src/scripts-header/github.js
@@ -0,0 +1,18 @@
+module.exports = isProd =>
+`// ==UserScript==
+// @name         GitHub 工具箱
+// @name:en      GitHub ToolBox
+// @version      1.0.0
+// @description  添加用VS Code阅读代码按钮(github1s)
+// @description:en  Read code with VS Code(github1s)
+// @author       sakura-flutter
+// @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
+// @license      MIT
+// @compatible   chrome Latest
+// @compatible   firefox Latest
+// @compatible   edge Latest
+// @noframes
+// @grant        window.onurlchange
+// @match        https://github.com/*
+// ==/UserScript==
+`
