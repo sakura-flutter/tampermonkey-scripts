6809bd5acf28136b432d152e4ad28c366eeaf367
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-03-17 14:47:16 +0800

    fix(mdn-web-docs): 修复按钮显示

--- src/scripts-header/mdn-web-docs.js
+++ src/scripts-header/mdn-web-docs.js
@@ -1,8 +1,8 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         MDN 文档辅助
-// @version      1.1.0
-// @description  在提供切换中文语言的页面自动切换为中文
+// @version      1.1.1
+// @description  在提供中文语言的页面自动切换为中文
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
