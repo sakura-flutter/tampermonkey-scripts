80e08a1b66b26f0ac17bed9d1d612ff3b5fa5a26
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-04-21 23:58:51 +0800

    Updates

--- pixiv.js
+++ pixiv.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixiv 工具箱
-// @version      1.2.0
-// @description  增强P站查看原图功能
+// @version      1.3.0
+// @description  增强P站查看原图功能；显示原图尺寸
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -37,7 +37,7 @@ function table(...args) {
 }
 
 
-;// CONCATENATED MODULE: ./src/scripts/pixiv/index.js
+;// CONCATENATED MODULE: ./src/scripts/pixiv/previewer.js
 function _classPrivateFieldLooseBase(receiver, privateKey) { if (!Object.prototype.hasOwnProperty.call(receiver, privateKey)) { throw new TypeError("attempted to use private field on non-instance"); } return receiver; }
 
 var id = 0;
@@ -48,23 +48,17 @@ function _classPrivateFieldLooseKey(name) { return "__private_" + id++ + "_" + n
 
 /* global Viewer */
 
-function main() {
-  GM_addStyle(GM_getResourceText('viewerCSS'));
-  GM_addStyle(['.viewer-backdrop { background-color: rgb(0 0 0 / 0.8) }', // 背景暗一点
-  '.viewer-container .viewer-title { text-shadow: 1px 1px 1px #000 }', // 添加标题阴影 在图片是白底时显示得清楚点
-  '.viewer-container .viewer-navbar ul, .viewer-container .viewer-navbar li { width: 66px; height: 110px }' // 加大导航栏
-  ].join('')); // eslint-disable-next-line no-new
-
-  new Previewer('figure [role="presentation"] a img', {
-    includePathname: /^\/artworks\/(\w)+/
-  });
-}
+GM_addStyle(GM_getResourceText('viewerCSS'));
+GM_addStyle(['.viewer-backdrop { background-color: rgb(0 0 0 / 0.8) }', // 背景暗一点
+'.viewer-container .viewer-title { text-shadow: 1px 1px 1px #000 }', // 添加标题阴影 在图片是白底时显示得清楚点
+'.viewer-container .viewer-navbar ul, .viewer-container .viewer-navbar li { width: 66px; height: 110px }' // 加大导航栏
+].join(''));
 
 var _el = _classPrivateFieldLooseKey("el");
 
-var _options = _classPrivateFieldLooseKey("options");
-
 var _viewer = _classPrivateFieldLooseKey("viewer");
+
+var _options = _classPrivateFieldLooseKey("options");
 
 var _init = _classPrivateFieldLooseKey("init");
 
@@ -92,17 +86,17 @@ class Previewer {
     });
     Object.defineProperty(this, _el, {
       writable: true,
-      value: void 0
+      value: null
+    });
+    Object.defineProperty(this, _viewer, {
+      writable: true,
+      value: null
     });
     Object.defineProperty(this, _options, {
       writable: true,
       value: {
         includePathname: null
       }
-    });
-    Object.defineProperty(this, _viewer, {
-      writable: true,
-      value: null
     });
     Object.defineProperty(this, _process, {
       writable: true,
@@ -129,9 +123,7 @@ class Previewer {
         if (originalArtworks.length === 0) return;
         event.preventDefault();
         event.stopPropagation();
-        event.stopImmediatePropagation(); // 释放上一次
-
-        _classPrivateFieldLooseBase(this, _viewer)[_viewer]?.destroy();
+        event.stopImmediatePropagation();
         _classPrivateFieldLooseBase(this, _viewer)[_viewer] = _classPrivateFieldLooseBase(this, _preview)[_preview](originalArtworks, {
           initialViewIndex: index
         });
@@ -150,12 +142,7 @@ var _init2 = function _init2() {
   window.addEventListener('click', _classPrivateFieldLooseBase(this, _process)[_process], true);
   window.addEventListener('urlchange', info => {
     warn('urlchange', info);
-
-    if (_classPrivateFieldLooseBase(this, _viewer)[_viewer]) {
-      _classPrivateFieldLooseBase(this, _viewer)[_viewer].destroy();
-
-      _classPrivateFieldLooseBase(this, _viewer)[_viewer] = null;
-    }
+    _classPrivateFieldLooseBase(this, _viewer)[_viewer]?.hide();
   });
 };
 
@@ -181,6 +168,7 @@ var _createOriginalImgEls2 = function _createOriginalImgEls2(imgEls) {
 };
 
 var _preview2 = function _preview2(imgEls, viewerOpts) {
+  const self = this;
   const container = document.createElement('div');
   container.append(...imgEls);
   viewerOpts = Object.assign({
@@ -192,6 +180,15 @@ var _preview2 = function _preview2(imgEls, viewerOpts) {
 
     viewed() {
       this.viewer.tooltip();
+    },
+
+    // 销毁
+    hide() {
+      _classPrivateFieldLooseBase(self, _viewer)[_viewer] = null;
+    },
+
+    hidden() {
+      this.viewer.destroy();
     }
 
   }, viewerOpts);
@@ -200,7 +197,74 @@ var _preview2 = function _preview2(imgEls, viewerOpts) {
   warn('viewer:', container, viewer);
   return viewer;
 };
+;// CONCATENATED MODULE: ./src/scripts/pixiv/pixels.js
 
-main();
+function attachPixels(el) {
+  const ws = new WeakSet();
+
+  function handler() {
+    $$(el).forEach(img => {
+      if (ws.has(img)) return; // 获取原尺寸
+
+      let [width, height] = [img.getAttribute('width'), img.getAttribute('height')];
+      if (width === null || height === null) return;
+      [width, height] = [+width, +height];
+      img.parentElement.style.position = 'relative';
+      const elem = createPixelsElement(img.parentElement);
+      elem.innerText = `${width} × ${height} (${calcRectPercent(width, height)})`;
+      ws.add(img);
+    });
+  }
+
+  const scheduling = function () {
+    let intervalId;
+    return function () {
+      clearInterval(intervalId);
+      if (document.visibilityState === 'hidden') return;
+      handler();
+      intervalId = setInterval(handler, 800);
+    };
+  }();
+
+  scheduling();
+  document.addEventListener('visibilitychange', scheduling);
+}
+
+function createPixelsElement(parentElement) {
+  const classname = 'artwork-pixels';
+
+  for (const child of parentElement.children) {
+    if (child.classList.contains(classname)) return child;
+  } // 没有则插入一个
+
+
+  const elem = document.createElement('span');
+  elem.classList.add(classname);
+  elem.style = ['position: absolute', 'z-index: 1', 'top: 32px', 'right: 8px', 'padding: 0 4px', 'border-radius: 8px', 'font-size: 12px', 'line-height: initial', 'color: #fff', 'background: rgb(0 0 0 / 0.32)'].join(';');
+  parentElement.prepend(elem);
+  return elem;
+} // 计算图片屏占比
+
+
+function calcRectPercent(width, height) {
+  const [sw, sh] = [screen.width, screen.height];
+  let ratio;
+
+  if (width >= sw || height >= sh) {
+    ratio = width / height / (sw / sh);
+  } else {
+    ratio = width * height / (sw * sh);
+  }
+
+  return (ratio * 100).toFixed(0) + '%';
+}
+;// CONCATENATED MODULE: ./src/scripts/pixiv/index.js
+
+ // eslint-disable-next-line no-new
+
+new Previewer('figure [role="presentation"] a img', {
+  includePathname: /^\/artworks\/(\w)+/
+});
+attachPixels('figure [role="presentation"] a img');
 /******/ })()
 ;
