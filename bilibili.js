82149f0b129fc652d9cdbe8c355692475dae4c14
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-05-10 12:23:29 +0800

    Updates

--- bilibili.js
+++ bilibili.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         bilibili 工具箱
-// @version      1.0.2
+// @version      1.0.3
 // @description  将播放页中视频快捷键在整个页面上生效
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
@@ -20,12 +20,13 @@ function attachVideoShortcutKeysInPage() {
   // jsc-player:formatted 约 37833、38867 行，事件疑似经过 M.event 3023 行包装
   // 关键在于把 containerFocus 设为 true
   // 或调用 seekFromArrowLeft 等函数
-  window.addEventListener('click', function () {
+  // fix: click 会在切换视频后失效，不知道为什么就是很奇怪😮，用 keydown 替代
+  window.addEventListener('keydown', () => {
     // 用第一种方式简单点
     unsafeWindow.$('.bilibili-player-video-wrap').trigger('click.bilibiliplayer');
-  });
+  }, true);
 }
 
-setTimeout(attachVideoShortcutKeysInPage, 1500);
+attachVideoShortcutKeysInPage();
 /******/ })()
 ;
