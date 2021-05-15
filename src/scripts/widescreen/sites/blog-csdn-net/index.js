c42c9364cd2a5eca0ab319b05ab277c33579a263
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-05-15 12:48:28 +0800

    feat(widescreen): csdn 登录弹窗隐藏优化

--- src/scripts/widescreen/sites/blog-csdn-net/index.js
+++ src/scripts/widescreen/sites/blog-csdn-net/index.js
@@ -2,6 +2,14 @@ import styles from './index.lazy.scss'
 
 export const csdn = ({ store, createControl }) => ({
   handler() {
-    styles.use()
+    createControl({
+      store,
+      execute() {
+        // 关闭登录弹窗
+        document.cookie = `unlogin_scroll_step=${Date.now()};domain=.csdn.net;path=/`
+
+        styles.use()
+      },
+    })
   },
 })
