603e39cd1964d9d2cb2e2c1aa5412860c8dd1d8e
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2021-07-11 01:48:38 +0800

    feat(bilibili): 长按 S 键倍速播放

--- src/scripts-header/bilibili.js
+++ src/scripts-header/bilibili.js
@@ -1,8 +1,8 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         bilibili 工具箱
-// @version      1.1.0
-// @description  将播放页中视频快捷键在整个页面上生效
+// @version      1.2.0
+// @description  将播放页中视频快捷键在整个页面上生效；长按 S 键倍速播放
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
---
+++ src/scripts/bilibili/speed.js
@@ -0,0 +1,44 @@
+import { $ } from '@/utils/selector'
+
+export default function speed() {
+  longPress('KeyS', () => {
+    const video = $('#bilibiliPlayer video')
+    const oldPlaybackRate = video.playbackRate
+    video.playbackRate = 3
+
+    window.addEventListener('keyup', () => {
+      video.playbackRate = oldPlaybackRate
+    }, { once: true })
+  })
+}
+
+/**
+ * 长按键盘
+ * @param {string} code keyCode
+ * @param {function} callback
+ * @param {number} duration 长按时间
+ */
+function longPress(code, callback, duration = 350) {
+  let timeoutID = null
+
+  window.addEventListener('keypress', event => {
+    if (event.code === code && timeoutID) return
+
+    if (event.code !== code) {
+      if (timeoutID) {
+        clearTimeout(timeoutID)
+        timeoutID = null
+      }
+      return
+    }
+
+    timeoutID = setTimeout(() => {
+      callback()
+    }, duration)
+
+    window.addEventListener('keyup', () => {
+      clearTimeout(timeoutID)
+      timeoutID = null
+    }, { once: true })
+  })
+}
