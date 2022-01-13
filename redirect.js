c2a845821a536fdc45309a0ed2f9d19f53d19d2d
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2022-01-13 14:58:43 +0800

    Updates

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         redirect 外链跳转
-// @version      1.23.0
-// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv、搜狗、Google、站长之家、OSCHINA、掘金、腾讯文档、pc6下载站、爱发电、Gitee
+// @version      1.24.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv、搜狗、Google、站长之家、OSCHINA、掘金、腾讯文档、pc6下载站、爱发电、Gitee、天眼查
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -35,6 +35,7 @@
 // @match        *://www.pc6.com/goread.html*
 // @match        *://afdian.net/link*
 // @match        *://gitee.com/link*
+// @match        *://www.tianyancha.com/security*
 // @include      /^https?:\/\/www\.google\..{2,7}url/
 // ==/UserScript==
 
@@ -344,7 +345,12 @@ const afdian = () => ({
 const gitee = () => ({
   query: 'target'
 });
+;// CONCATENATED MODULE: ./src/scripts/redirect/sites/www-tianyancha-com.js
+const tianyancha = () => ({
+  query: 'target'
+});
 ;// CONCATENATED MODULE: ./src/scripts/redirect/sites/index.js
+
 
 
 
@@ -479,6 +485,10 @@ const sites = [{
   name: 'Gitee',
   test: 'gitee.com/link',
   use: gitee
+}, {
+  name: '天眼查',
+  test: 'www.tianyancha.com/security',
+  use: tianyancha
 }];
 /* harmony default export */ const redirect_sites = (sites);
 ;// CONCATENATED MODULE: ./src/scripts/redirect/index.js
