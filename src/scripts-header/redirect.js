2c652605766e4180d1aa709bda5a8294af492410
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2022-01-13 14:58:12 +0800

    feat(redirect): 适配 天眼查

--- src/scripts-header/redirect.js
+++ src/scripts-header/redirect.js
@@ -1,8 +1,8 @@
 module.exports = isProd =>
 `// ==UserScript==
 // @name         redirect 外链跳转
-// @version      1.23.0
-// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv、搜狗、Google、站长之家、OSCHINA、掘金、腾讯文档、pc6下载站、爱发电、Gitee
+// @version      1.24.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv、搜狗、Google、站长之家、OSCHINA、掘金、腾讯文档、pc6下载站、爱发电、Gitee、天眼查
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -36,6 +36,7 @@ module.exports = isProd =>
 // @match        *://www.pc6.com/goread.html*
 // @match        *://afdian.net/link*
 // @match        *://gitee.com/link*
+// @match        *://www.tianyancha.com/security*
 // @include      ${/^https?:\/\/www\.google\..{2,7}url/}
 // ==/UserScript==
 `
