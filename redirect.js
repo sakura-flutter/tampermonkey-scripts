6b11787ba34204dcef19391281f59be718faf1c0
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2022-05-15 23:13:19 +0800

    Updates

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         redirect 外链跳转
-// @version      1.27.0
+// @version      1.28.0
 // @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv、搜狗、Google、站长之家、OSCHINA、掘金、腾讯文档、pc6下载站、爱发电、Gitee、天眼查、爱企查、企查查、优设网、51CTO、力扣
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
@@ -40,7 +40,7 @@
 // @match        *://www.qcc.com/web/transfer-link*
 // @match        *://link.uisdc.com/*
 // @match        *://blog.51cto.com/transfer*
-// @match        *://leetcode-cn.com/link*
+// @match        *://leetcode.cn/link*
 // @include      /^https?:\/\/www\.google\..{2,7}url/
 // ==/UserScript==
 
@@ -532,7 +532,7 @@ const sites = [{
   use: cto51
 }, {
   name: '力扣',
-  test: 'leetcode-cn.com/link/',
+  test: 'leetcode.cn/link/',
   use: () => ({
     query: 'target'
   })
