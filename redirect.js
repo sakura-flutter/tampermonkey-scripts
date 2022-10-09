c08ef3fdf683df40db864368771ee394ce5f530e
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2022-10-09 14:15:13 +0800

    Updates

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         redirect 外链跳转
-// @version      1.31.0
-// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv、搜狗、Google、站长之家、OSCHINA、掘金、腾讯文档、pc6下载站、爱发电、Gitee、天眼查、爱企查、企查查、优设网、51CTO、力扣
+// @version      1.32.0
+// @description  自动跳转(重定向)到目标链接，免去点击步骤。适配了简书、知乎、微博、QQ邮箱、QQPC、印象笔记、贴吧、CSDN、YouTube、微信、微信开放社区、开发者知识库、豆瓣、个人图书馆、Pixiv、搜狗、Google、站长之家、OSCHINA、掘金、腾讯文档、pc6下载站、爱发电、Gitee、天眼查、爱企查、企查查、优设网、51CTO、力扣、花瓣网
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -41,6 +41,7 @@
 // @match        *://link.uisdc.com/*
 // @match        *://blog.51cto.com/transfer*
 // @match        *://leetcode.cn/link*
+// @match        *://huaban.com/go*
 // @include      /^https?:\/\/www\.google\..{2,7}url/
 // ==/UserScript==
 
@@ -278,6 +279,7 @@ const pixiv = () => {
 
 
 
+
 const sites = [{
   name: '简书',
   test: 'www.jianshu.com/go-wild',
@@ -466,6 +468,16 @@ const sites = [{
   use: () => ({
     query: 'target'
   })
+}, {
+  name: '花瓣网',
+  test: 'huaban.com/go',
+  readyState: 'interactive',
+  use: () => {
+    const nextData = JSON.parse($('#__NEXT_DATA__').textContent);
+    return {
+      link: nextData.props.pageProps.data.link
+    };
+  }
 }];
 /* harmony default export */ const redirect_sites = (sites);
 ;// CONCATENATED MODULE: ./src/scripts/redirect/index.ts
