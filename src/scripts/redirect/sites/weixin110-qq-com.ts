82ec4f8b8ba24b592c405db11fa6409cec71dd47
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2022-11-19 09:25:07 +0800

    feat(redirect): 更新 weixin

--- src/scripts/redirect/sites/weixin110-qq-com.ts
+++ src/scripts/redirect/sites/weixin110-qq-com.ts
@@ -1,20 +1,21 @@
 /* eslint-disable camelcase */
 import { parse } from '@/utils/querystring'
+import type { Site } from '../types'
 
-export const weixin = () => {
+export const weixin: Site['use'] = () => {
   const { main_type } = parse()
   switch (main_type) {
+    case '2': {
+      const url = new URL(location.href)
+      // 转为 1 可还原链接
+      url.searchParams.set('main_type', '1')
+      location.replace(url.href)
+      return {}
+    }
     case '1':
+    default:
       return {
         selector: '.weui-msg__text-area .ui-ellpisis-content p',
       }
-    case '2': {
-      const url = new URL(location.href)
-      // 转为1可还原链接
-      url.searchParams.set('main_type', '1')
-      location.replace(url.href)
-    }
   }
-
-  return {}
 }
