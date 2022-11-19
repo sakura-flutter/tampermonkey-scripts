215d53e511e00dd8024d3e2bd086225425de70f8
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2022-11-19 12:16:47 +0800

    feat(redirect): 更新 weixin

--- src/scripts/redirect/sites/weixin110-qq-com.ts
+++ src/scripts/redirect/sites/weixin110-qq-com.ts
@@ -2,8 +2,14 @@
 import { parse } from '@/utils/querystring'
 import type { Site } from '../types'
 
+const { atob } = window
+
 export const weixin: Site['use'] = () => {
-  const { main_type } = parse()
+  const { main_type, midpagecode } = parse()
+
+  /**
+   * main_type 貌似是旧的规则
+   */
   switch (main_type) {
     case '2': {
       const url = new URL(location.href)
@@ -13,9 +19,27 @@ export const weixin: Site['use'] = () => {
       return {}
     }
     case '1':
-    default:
-      return {
-        selector: '.weui-msg__text-area .ui-ellpisis-content p',
-      }
+      break
+  }
+
+  /**
+   * midpagecode 似乎是新的规则
+   */
+  const MAGIC_KEY = atob(atob('Tmpjek56ZGhNbUZrWWpRMFpURTNZekZpTUdGa1lqSTBZalZqWmpKaVpERXlZek0wWkRsaU5UWmxNRFpqWTJRMlpHUTBZekk1TVdJME1qTmlOV0prTjJabU5tUmhZbVJqTlRVM1l6azVNbVkxWkRZd1pEZzVNbUkyT0Rjd1pqYzBOakV3TldNM05HRmhNalJqTXpBMk0yUTNOR1ExT1dJMFlXVTFOVFF6WldJM1lqSmtObVUwT1dOak1qYzNNMkZsTVRjM01UWTNNemcwTmpRM04ySmpOalppTTJNelltUTNPVE5sWkRJNFpEZGhaVE5rTnpZeE0yUm1ZVGRpWW1ReQ=='))
+  if (
+    midpagecode &&
+    midpagecode !== MAGIC_KEY &&
+    !(window as any).cgiData?.url
+  ) {
+    const url = new URL(location.href)
+    // 会还原链接
+    url.searchParams.set('midpagecode', MAGIC_KEY)
+    location.replace(url.href)
+    return {}
+  }
+
+  return {
+    // 如果解析得到，会出现在页面这里
+    selector: '.weui-msg__text-area .ui-ellpisis-content p',
   }
 }
