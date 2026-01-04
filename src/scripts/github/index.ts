c6f776e77d5a5c12146c8538239545550948bbae
shangxueink <1919892171@qq.com>
2026-01-04 17:11:55 +0800

    style(github): improve GitHub1s button HTML formatting and indentation
    Reorganize button HTML with better formatting and multi-line structure.
    Add GitHub1s icon image to button with proper styling attributes.
    Adjust indentation for onmouseenter event handler for consistency.

--- src/scripts/github/index.ts
+++ src/scripts/github/index.ts
@@ -4,13 +4,18 @@ function insert1sButton() {
   const actions = $('.pagehead-actions')
   if (actions == null || $('#github1s-button')) return
 
-  const btnHTML = '<li><a id="github1s-button" class="btn btn-sm" target="_blank">GitHub1s</a></li>'
+  const btnHTML = `<li>
+    <a id="github1s-button" class="btn btn-sm" target="_blank">
+      <img src="https://github1s.com/favicon-light.svg" alt="" height="16" width="16" class="v-align-text-bottom d-inline-block mr-2" style="vertical-align: text-bottom;">
+      <span class="d-inline">GitHub1s</span>
+    </a>
+  </li>`
   actions.insertAdjacentHTML('afterbegin', btnHTML)
-  ;($('#github1s-button') as HTMLAnchorElement).onmouseenter = function(this: HTMLAnchorElement) {
-    const github1sURL = new URL(location.href)
-    github1sURL.host = 'github1s.com'
-    this.href = github1sURL.href
-  } as any
+    ; ($('#github1s-button') as HTMLAnchorElement).onmouseenter = function (this: HTMLAnchorElement) {
+      const github1sURL = new URL(location.href)
+      github1sURL.host = 'github1s.com'
+      this.href = github1sURL.href
+    } as any
 }
 
 insert1sButton()
