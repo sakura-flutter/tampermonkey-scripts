b07a96a8b26db734bdb2f16988ca3baa15000e78
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2026-01-10 17:18:49 +0800

    Updates

--- playback-rate.js
+++ playback-rate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         视频倍速播放
-// @version      1.0.0
-// @description  长按 [0、`(反引号键)] 键 3 倍速，双击长按 6 倍速。适配了哔哩哔哩、抖音、小红书、知乎、极客时间、YouTube、爱奇艺、优酷、PPTV、芒果TV、乐视视频、搜狐视频、咪咕视频
+// @version      1.1.0
+// @description  长按 [0、`(反引号键)] 键 3 倍速，双击长按 6 倍速。适配了哔哩哔哩、抖音、小红书、知乎、微博、极客时间、YouTube、爱奇艺、优酷、PPTV、芒果TV、乐视视频、搜狐视频、咪咕视频
 // @author       sakura-flutter
 // @namespace    https://github.com/sakura-flutter/tampermonkey-scripts
 // @license      GPL-3.0
@@ -9,6 +9,7 @@
 // @match        *://www.douyin.com/*
 // @match        *://www.xiaohongshu.com/*
 // @match        *://www.zhihu.com/*
+// @match        *://*.weibo.com/*
 // @match        *://time.geekbang.org/*
 // @match        *://www.youtube.com/*
 // @match        *://www.iqiyi.com/*
